bb62df3b2290cbe281fb75ee75ea2caba81f9941
shardakova svetlana <shardakova.s@medlinx.online>
2023-05-20 17:33:21 +0300

    Initial Commit

---
+++ StrechedHeader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StrechedHeader
+//
+//  Created by Svetlana Shardakova on 20.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
